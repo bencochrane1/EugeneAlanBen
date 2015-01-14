46c48e28634ea625778e2fa64c93ada61d4e0b70
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-01-14 14:05:24 +1100

    added delete but it doesnt work

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -35,7 +35,7 @@ class ProjectsController < ApplicationController
     end
 
     def destroy
-        @project.destroy
+        @current_project.destroy
         redirect_to projects_path
     end
 
