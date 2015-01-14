cc35c63e356187777682d200476e4bdfcaa25219
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2015-01-14 17:52:24 +1100

    stuff

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,7 +1,7 @@
 class LessonsController < ApplicationController
   def index
     @projects = current_project.all
-    @lessons = @projects.lesson.all
+    @lessons = @projects.lessons.all
     redirect_to project_lesson_path(@project, lesson)
   end
 
@@ -15,36 +15,36 @@ class LessonsController < ApplicationController
   def create 
     @lesson = current_project.lessons.create(lesson_params)
     if @lesson.save
-            redirect_to project_lessons_path, notice: "Lesson created"
-        else
-            render :new
-        end
+      redirect_to project_lessons_path, notice: "Lesson created"
+    else
+      render :new
+    end
   end
 
-    def show
-      @project = current_account.projects.find(params[:id])
-      @lesson = @project.lessons.find(params[:id])
-    end
-
-    def edit
-      @project = current_account.projects.find(params[:id])
-      @lesson = @project.lessons.find(params[:id])    
-    end
-
-    def update
-       @project = current_account.projects.find(params[:id])
-       @lesson = @project.lessons.find(params[:id]) 
-       
-        if @lesson.update(lesson_params)
-            redirect_to project_lesson_path, notice: "Lesson updated"
-        else 
-            render :edit
-        end
-    end
-
-  private
-
-  def lesson_params
-    params.require(:lesson).permit(:name, :video, :description, :pdf, :project_id)
+  def show
+    @project = current_account.projects.find(params[:project_id])
+    @lesson = @project.lessons.find(params[:id])
   end
+
+  def edit
+    @project = current_account.projects.find(params[:id])
+    @lesson = @project.lessons.find(params[:id])    
+  end
+
+  def update
+   @project = current_account.projects.find(params[:id])
+   @lesson = @project.lessons.find(params[:id]) 
+
+   if @lesson.update(lesson_params)
+    redirect_to project_lesson_path, notice: "Lesson updated"
+  else 
+    render :edit
+  end
+end
+
+private
+
+def lesson_params
+  params.require(:lesson).permit(:name, :video, :description, :pdf, :project_id)
+end
 end
