787b056af20eb8089187976c2ef54bf8f89fc852
Ben Cochrane <bencochrane1@me.com>
2015-01-14 15:27:40 +1100

    add in lesson view and edit

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,6 +1,8 @@
 class LessonsController < ApplicationController
   def index
-    @lessons = current_account.lessons.all
+    @projects = current_project.all
+    @lessons = @projects.lesson.all
+    redirect_to project_lesson_path(@project, lesson)
   end
 
   def new
@@ -13,11 +15,32 @@ class LessonsController < ApplicationController
   def create 
     @lesson = current_project.lessons.create(lesson_params)
     if @lesson.save
-            redirect_to root_path, notice: "Lesson created"
+            redirect_to project_lessons_path, notice: "Lesson created"
         else
             render :new
         end
   end
+
+    def show
+      @project = current_account.projects.find(params[:id])
+      @lesson = @project.lessons.find(params[:id])
+    end
+
+    def edit
+      @project = current_account.projects.find(params[:id])
+      @lesson = @project.lessons.find(params[:id])    
+    end
+
+    def update
+       @project = current_account.projects.find(params[:id])
+       @lesson = @project.lessons.find(params[:id]) 
+       
+        if @lesson.update(lesson_params)
+            redirect_to project_lesson_path, notice: "Lesson updated"
+        else 
+            render :edit
+        end
+    end
 
   private
 
