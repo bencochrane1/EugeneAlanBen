4f1b8653146d5014f82aa35c4b94ccc22f4e82e6
Ben Cochrane <bencochrane1@me.com>
2015-01-15 23:38:14 +1100

    add in stripe api for subscription payments

---
+++ app/controllers/subscribe_controller.rb
@@ -0,0 +1,29 @@
+class SubscribeController < ApplicationController
+    before_filter :authenticate_user!
+
+    def new
+    end
+
+    def update
+        #gets the credit card details submitted in the form
+        token = params[:stripeToken]
+
+        #create a customer
+        customer = Stripe::Customer.create(
+            card: token,
+            plan: 1095,
+            email: current_user.email
+            )
+
+        current_user.subscribed = true
+        current_user.stripeid = customer.id
+        current_user.save
+
+
+        redirect_to projects_path, notice: "Your subscription was set up successfully! Happy learning!"
+
+    end
+
+
+
+end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,8 @@
 require 'stripe'
 
-Stripe.api_key = "sk_test_1CFvYdc0wCQdSS1KU31siae6"
+Rails.configuration.stripe = {
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
