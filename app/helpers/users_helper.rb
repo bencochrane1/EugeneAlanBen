99552a0d66ac04db0fd511eeb72d703ff8bb04af
Alan <alanng95@hotmail.com>
2015-01-16 12:53:52 +1100

    disqus

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
     def user_status(user)
-        if current_account.owner == user
+        if current_account.owner
             content_tag(:span, '', class: 'glphyicon glphyicon-ok color-success')
-        else
-            'Invitation Pending'
-        end
+            'Admin'
+        else 
+            content_tag(:span, '', class: 'glphyicon glphyicon-ok color-success')
+            'Student'
+          end
     end
 end
